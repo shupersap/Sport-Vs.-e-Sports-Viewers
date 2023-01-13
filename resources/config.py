97dc7d6a34883aebd9218f4f5a419c7c56378d01
benbowlingGT <110581635+benbowlingGT@users.noreply.github.com>
2023-01-12 19:52:42 -0500

    Add files via upload

---
+++ resources/config.py
@@ -0,0 +1,40 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "c5a7c1a8",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "\n",
+    "POSTGRES_ADDRESS = 'localhost' ## INSERT YOUR DB ADDRESS IF IT'S NOT ON PANOPLY\n",
+    "POSTGRES_PORT = '5432'\n",
+    "POSTGRES_USERNAME = 'postgres' ## CHANGE THIS TO YOUR PANOPLY/POSTGRES USERNAME\n",
+    "POSTGRES_PASSWORD = 'Bowling2014' ## CHANGE THIS TO YOUR PANOPLY/POSTGRES PASSWORD\n",
+    "POSTGRES_DBNAME = 'sports_viewership' ## CHANGE THIS TO YOUR DATABASE NAME\n"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.12"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
